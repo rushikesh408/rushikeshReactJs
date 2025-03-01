d2c2b2ff8af7aead904d0cdf9e78f5a735eeadec
rushikesh408 <rushikeshveeramally@gmail.com>
2025-02-28 21:27:53 -0500

    Learnt Edit and Delete user functionality

--- src/Users/UserList.js
+++ src/Users/UserList.js
@@ -7,6 +7,15 @@ function UserList() {
   let [users, setUsers] = useState([]);
   let [filteredUsers, setFilteredUsers] = useState([]);
   let userInputData = "";
+  let [id, setId] = useState("")
+  let [firstName, setFirstName] = useState("")
+  let [email, setEmail] = useState("")
+  let [profileImage, setProfileImage] = useState("")
+
+
+
+
+  
 
   useEffect(() => {
     async function getUsers() {
@@ -17,6 +26,27 @@ function UserList() {
     }
     getUsers();
   }, []);
+
+
+
+
+function handleEditUser(user){
+
+  setId(user.id)
+  setEmail(user.email)
+  setFirstName(user.firstName)
+  setProfileImage(user.image)
+
+}
+
+
+
+
+
+
+
+
+
 
   function filterd(e) {
     let userInputted = e.target.value;
@@ -33,15 +63,26 @@ function UserList() {
   }
 
   function handleSort() {
-    console.log("clicked here")
+    console.log("clicked here");
     let tempData = users.sort((user1, user2) => {
       if (user1.firstName.toLowerCase() < user2.firstName.toLowerCase()) {
         return -1;
       } else {
         return 1;
       }
-    })
-setUsers([...tempData])
+    });
+    setUsers([...tempData]);
+  }
+
+  async function handleUserDelete(user) {
+    let apicallToDeleteUser = await axios.delete(
+      "https://dummyjson.com/users/" + user.id
+    );
+
+    let tempData = users.filter((deletedUser) => {
+      return deletedUser.id != user.id;
+    });
+    setUsers([...tempData]);
   }
 
   return (
@@ -59,29 +100,64 @@ setUsers([...tempData])
         />
       </div>
 
-      <div className="row mt-5 mb-5">
-        <table className="table table-secondary rounded mt-20">
+      <div className="row mt-5 mb-5 col-12">
+        <table className="table table-secondary rounded mt-20 col-8 ">
           <thead>
             <tr>
+              <th>ID</th>
               <th scope="col" onClick={(event) => handleSort()}>
                 FirstName
               </th>
               <th scope="col">Email</th>
               <th scope="col">Profile Photo</th>
+              <th scope="col">Actions</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user) => (
               <tr>
+                <td>{user.id}</td>
                 <td>{user.firstName}</td>
                 <td>{user.email}</td>
                 <td>
                   <img src={user.image}></img>
                 </td>
+                <td>
+                  <button
+                    className="btn btn-warning mx-3"
+                    onClick={(event) => handleUserDelete(user)}
+                  >
+                    Delete
+                  </button>
+                  <button className="btn btn-warning" onClick={event=> handleEditUser(user)}>edit</button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
+
+        <form className="col-4">
+          <div className="form-group ">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" value={email} />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">ID</label>
+            <input type="text" className="form-control" value={id}/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">FirstName</label>
+            <input type="text" className="form-control" value={firstName}/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Profile Photo</label>
+            <img src={profileImage}/>
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
 
       <div className="row mt-5 mb-5"></div>
